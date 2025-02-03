10754a508fc9f520b4b0bf9a5199903befee134d
Christian Troth <trothchristian@gmail.com>
2025-02-03 14:48:43 -0500

    completed the task deletion option.  refactored code to utilize two additional functions.  one to delete a task and one to output the current to do list.

--- main.py
+++ main.py
@@ -6,16 +6,16 @@
 
 import sys
 import json
-import rich
-from rich import print
-from rich.console import Console
-from rich.panel import Panel
-from rich.table import Table
-from rich.prompt import Prompt
+import rich #import the rich module which is used to make the output look better
+from rich import print #import the print function from the rich module
+from rich.console import Console #import the Console class from the rich module
+from rich.panel import Panel #import the Panel class from the rich module
+from rich.table import Table #import the Table class from the rich module which is used to display data in a table format
+from rich.prompt import Prompt #import the Prompt class from the rich module
 
 console = Console() #create a a Console class object called console
-to_do_list = {}
-task_number = 0
+to_do_list = {} #create an empty dictionary called to_do_list
+task_number = 0 #create a task number variable and set it to 0
 
 
 def main():
@@ -24,40 +24,37 @@ def main():
     global task_number
 
     
-    name = Prompt.ask("Enter your name").lower().title() #asks the user for name.  sets all characters to lower case and then sets the first character to uppercase via title
-    
-    while True:
+    name = Prompt.ask("\nEnter your name").lower().title() #asks the user for name.  sets all characters to lower case and then sets the first character to uppercase via title
+    console.print(f"\nWelcome {name} to your 'To Do List'\n") #prints a welcome message to the user with their name
+
+    while True: #infinite loop that will continue until the user selects the exit option
 
         console.print(Panel.fit(f"\n[bold green]--- {name}'s To Do List ---[/bold green]",title="Configuration Menu", )) #creates a box with Configuration Menu at the top.  In the center is displayed the user's name and To Do List
-        console.print("1. Enter a new task\n2. View Current To Do List\n3. Remove task from To Do List\n4. List taks in order of priority\n5. Export 'To Do List' to JSON file\n6. Exit\n") #asks the user what they want to do
+        console.print("\n1. Enter a new task\n2. View Current To Do List\n3. Remove task from To Do List\n4. List taks in order of priority\n5. Export 'To Do List' to JSON file\n6. Exit\n") #asks the user what they want to do
 
         choice = Prompt.ask("Select an option (1-6)", choices =[str(i) for i in range(1,7)]) #asks the user to input a number matching one of the above choices.  if it is not one of them an error is thrown stating Please select one of the available options. this is done via 'choices'
+        console.print("\n") #prints a new line
 
-        if choice == '1': #COMPLETED
+        if choice == '1': #COMPLETED - #lets the user add a new task to the to do list
             priority = Prompt.ask("Enter your task priority (1-10) with 1 being the most important and 10 being the least", choices = [str(i) for i in range(1,11)],)
             #the above priority variable asks the user to input a number between 1 and 10.  if it is not one of them an error is thrown stating Please select one of the available options. this is done via 'choices'
-            task = Prompt.ask("Enter a description of your task") #asks the user to input a description of the task
+            console.print("\n") #prints a new line
+            task = Prompt.ask("Enter a description of your task\n") #asks the user to input a description of the task
             task_number += 1 #increments the task number by 1
 
             to_do_list[task_number] = [priority, task] #adds the task to the to do list with the task number as the key and the priority and task as the values
 
-            console.print(f"Your current 'To Do List' is as follows -\n") #prints the current to do list in order it was created
-            for value in to_do_list.values(): #iterates through the values in the to do list - the values are the priority and task
-                console.print(value[1]) #prints the task
+            output_current_to_do_list() #calls the output_current_to_do_list function to display the current to do list
             console.print("\n") #prints a new line
 
 
-        elif choice == '2': #COMPLETED
-            console.print(f"Your current 'To Do List' is as follows -\n") #prints the current to do list in order it was created
-            count = 1 #count variable to increment the number of tasks
-            for value in to_do_list.values(): #iterates through the values in the to do list - the values are the priority and task
-                console.print(f"{count}. {value[1].title()}\n") #prints the task with the count number in front of it.  uses title to capitalize the first letter of each word
-                count += 1 #increments the count variable by 1
+        elif choice == '2': #COMPLETED - outputs current to do list
+            output_current_to_do_list()
 
-        elif choice == '3':
-            pass
+        elif choice == '3': #COMPLETED - lets the user delete a task from the to do list
+            del_task() #calls the del_task function that is defined below
 
-        elif choice == '4': #COMPLETED
+        elif choice == '4': #COMPLETED - lists tasks in order of priority
             console.print(f"Your current 'To Do List' in order of priority is as follows -\n") #prints the current to do list in order of priority
             count = 1 #count variable to increment the number of tasks
             list_length = len(to_do_list) #gets the length of the to do list, not currently used
@@ -71,8 +68,24 @@ def main():
         elif choice == '5':
             pass
 
-        elif choice == '6': #COMPLETED
-            print(f"Exiting your 'To Do List'...")
+        elif choice == '6': #COMPLETED - exits the program
+            print(f"Exiting your 'To Do List' {name}\n") #prints a message to the user that they are exiting the program
             exit() #exits the program
+
+def del_task(): #COMPLETED - lets the user delete a task from the to do list
+    task_deletion = Prompt.ask("Enter the task number you would like to delete\n") #asks the user to input the task number they would like to delete
+    if int(task_deletion) in to_do_list: #checks if the task number is in the to do list
+        del to_do_list[int(task_deletion)] #deletes the task from the to do list
+        console.print(f"Task {task_deletion} has been deleted\n") #prints a message to the user that the task has been deleted
+    else: #if the task number is not in the to do list
+        console.print(f"Task {task_deletion} does not exist\n") #prints a message to the user that the task does not exist
+    output_current_to_do_list() #calls the output_current_to_do_list function to display the current to do list
+
+def output_current_to_do_list(): #COMPLETED - outputs the current to do list
+    console.print(f"Your current 'To Do List' is as follows -\n") #prints the current to do list
+    count = 1 #count variable to increment the number of tasks
+    for value in to_do_list.values(): #iterates through the values of the to do list
+        console.print(f"{count}. {value[1].title()}\n") #prints the task with the count in front of it.  uses title to capitalize the first letter of each word
+        count += 1 #increments the count by 1
 
 main()
