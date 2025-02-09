2d5ef76a5750184f43f67b60c9b4b19db63bb83e
ctroth <trothchristian@gmail.com>
2025-02-09 01:12:58 -0500

    fixed all issues i noted in my notes.txt file.  it all appears to work without issue.  will continue to work on README.md

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ task_number = 0 #create a task number variable and set it to 0
 name = "" #create a name variable and set it to an empty string
 
 
+
 def main():
 
     global to_do_list #sets the to do list variable as a global variable that can be used as if it was in scope
@@ -40,55 +41,75 @@ def main():
         console.print(Panel(
             subtitle=f"[bold green]--- {name}'s 'To Do List' ---[/bold green]",
             title="[bold yellow]--- Configuration Menu --- [/bold yellow]",
-            renderable="[bold white]\n1. Enter A New Task\n2. View Current 'To Do List'\n3. Delete Task From 'To Do List'\n4. Update Task In 'To Do List'\n5. View 'To Do List' Priority\n6. Export 'To Do List' to JSON File\n7. Load JSON File\n8. Exit [/bold white]")) #prints a panel with the configuration menu options.  uses title, subtitle, and renderable to display the menu.  Renderable is used to display the menu options.  The word renderable is used to display the menu options in a different color
+            renderable="[bold white]\n1. Enter A New Task\n2. View Current 'To Do List'\n3. Delete Task From 'To Do List'\n4. Update Task In 'To Do List'\n5. Update Task Priority\n6. View 'To Do List' Priority\n7. Export 'To Do List' to JSON File\n8. Load JSON File\n9. Exit [/bold white]")) #prints a panel with the configuration menu options.  uses title, subtitle, and renderable to display the menu.  Renderable is used to display the menu options.  The word renderable is used to display the menu options in a different color
         console.print("\n") #prints a new line
 
-        choice = Prompt.ask("Select an option (1-8)", choices =[str(i) for i in range(1,9)]) #asks the user to input a number matching one of the 
+        choice = Prompt.ask("Select an option (1-8)", choices =[str(i) for i in range(1,10)]) #asks the user to input a number matching one of the 
         #above choices.  if it is not one of them, an error is thrown stating please select one of the available options. this is done via 'choices'
         console.print("\n") #prints a new line
 
         if choice == '1': #COMPLETED - #lets the user add a new task to the to do list
-            priority = Prompt.ask("Enter your task priority (1-10) with 1 being the most important and 10 being the least", choices = [str(i) for i in range(1,11)],)
-            #the above priority variable asks the user to input a number between 1 and 10.  if it is not one of them an error is thrown stating Please select one of the available options. this is done via 'choices'
-            console.print("\n") #prints a new line
-            task = Prompt.ask("Enter a description of your task") #asks the user to input a description of the task
-            while not all(word.isalpha() for word in task.split()): #checks if all the words in the task are alphabetic characters
-                console.print("Please enter a valid description of your task\n") #prints a message to the user to enter a valid task
-                task = Prompt.ask("Enter a description of your task")
-            console.print("\n") #prints a new line
-            task_number += 1 #increments the task number by 1
+            if task_number == 0: #checks if the task number is 0
+                priority = Prompt.ask("Enter your task priority (1-10) with 1 being the most important and 10 being the least", choices = [str(i) for i in range(1,11)],)
+                #the above priority variable asks the user to input a number between 1 and 10.  if it is not one of them an error is thrown stating Please select one of the available options. this is done via 'choices'
+                console.print("\n") #prints a new line
+                task = Prompt.ask("Enter a description of your task") #asks the user to input a description of the task
+                while not all(word.isalpha() for word in task.split()): #checks if all the words in the task are alphabetic characters
+                    console.print("Please enter a valid description of your task\n") #prints a message to the user to enter a valid task
+                    task = Prompt.ask("Enter a description of your task")
+                console.print("\n") #prints a new line
+                track_task_list_number() #calls the track_task_list_number function to track the task list number
+                to_do_list[task_number] = [priority, task] #adds the task to the to do list with the task number as the key and the priority and task as the values
+                to_do_list_table() #calls the to_do_list_table function to display the current to do list
+                console.print("\n") #prints a new line
 
-            to_do_list[task_number] = [priority, task] #adds the task to the to do list with the task number as the key and the priority and task as the values
-
-            to_do_list_table() #calls the to_do_list_table function to display the current to do list
-            console.print("\n") #prints a new line
-
-
+            elif task_number > 0: #checks if the task number is greater than 0
+                priority = Prompt.ask("Enter your task priority (1-10) with 1 being the most important and 10 being the least", choices = [str(i) for i in range(1,11)],)
+                #the above priority variable asks the user to input a number between 1 and 10.  if it is not one of them an error is thrown stating Please select one of the available options. this is done via 'choices'
+                console.print("\n") #prints a new line
+                task = Prompt.ask("Enter a description of your task") #asks the user to input a description of the task
+                while not all(word.isalpha() for word in task.split()): #checks if all the words in the task are alphabetic characters
+                    console.print("Please enter a valid description of your task\n") #prints a message to the user to enter a valid task
+                    task = Prompt.ask("Enter a description of your task") #asks the user to input a description of the task
+                console.print("\n") #prints a new line
+                track_task_list_number() #calls the track_task_list_number function to track the task list number
+                to_do_list[task_number] = [priority, task] #adds the task to the to do list with the task number as the key and the priority and task as the values
+                to_do_list_table() #calls the to_do_list_table function to display the current to do list
+                console.print("\n") #prints a new line
+           
         elif choice == '2': #COMPLETED - outputs current to do list
             to_do_list_table() #calls the to_do_list_table function to display the current to do list
 
         elif choice == '3': #COMPLETED - lets the user delete a task from the to do list
             del_task() #calls the del_task function that is defined below
+            task_number = task_number - 1 #decrements the task number by 1
         
         elif choice == '4':
             update_task() #calls the update_task function that is defined below
 
         elif choice == '5': #COMPLETED - lists tasks in order of priority
+            update_priority() #calls the update_priority function that is defined below
+
+        elif choice == '6': #COMPLETED - lists tasks in order of priority
             to_do_list_priority_table() #calls the to_do_list_priority_table function that is defined below   
         
-        elif choice == '6': #COMPLETED - exports to do list to JSON file
+        elif choice == '7': #COMPLETED - exports to do list to JSON file
             create_json_file() #calls the create_json_file function that is defined below
             output_user_json_file() #calls the output_user_json_file function that is defined below
 
-        elif choice == '7': #COMPLETED - loads a JSON file
+        elif choice == '8': #COMPLETED - loads a JSON file
             load_json_file() #calls the load_json_file function that is defined below
 
-        elif choice == '8': #COMPLETED - exits the program
+        elif choice == '9': #COMPLETED - exits the program
+            create_json_file() #calls the create_json_file function that is defined below
+            print(f"Your 'To Do List' has been saved to a JSON file called 'user_to_do_list.json'\n") #prints a message to the user that the to do list has been saved to a JSON file
+            print(f"If you want to load this 'To Do List' in the future, select option 8 from the menu\n") #prints a message to the user that they can load the to do list in the future by selecting option 7 from the menu
             print(f"Exiting your 'To Do List' {name}\n") #prints a message to the user that they are exiting the program
             exit() #exits the program
 
 def del_task(): #COMPLETED - lets the user delete a task from the to do list
     global to_do_list #sets the to do list variable as a global variable that can be used as if it was in scope
+    current_task_number = len(to_do_list) #gets the current task number by getting the length of the to do list
     if len(to_do_list) == 0: # checks if the length of the 'To Do List' is 0
         console.print("[bold red]THERE IS NO TASK TO DELETE![/bold red]\n") #prints a message to the user that there is no task to delete
         return
@@ -116,6 +137,9 @@ def create_json_file(): #function to create a JSON file
         return #returns to the main function
     else: #if there are tasks in the to do list then the following code is executed
         user_json_file = json.dumps(to_do_list, indent=4) #converts the to do list to a JSON file with an indent of 4
+        with open("user_to_do_list.json", "w") as file: #opens the JSON file in write mode because we are writing to it and sets it as a variable called file
+            file.write(user_json_file) #writes the to do list to the JSON file
+    
 
 def output_user_json_file(): #function to output the JSON file
     console.print(f"Your 'To Do List' has been saved to a JSON file called 'user_to_do_list.json'\n") #prints a message to the user that the to do list has 
@@ -125,9 +149,13 @@ def output_user_json_file(): #function to output the JSON file
 
 def load_json_file(): #function to load a previous JSON file or JSON file to use in the 'To Do List' python program
     global to_do_list #sets the to do list variable as a global variable that can be used as if it was in scope
+    global task_number #sets the task number variable as a global variable that can be used as if it was in scope
     try:
         with open("user_to_do_list.json", "r") as file: #opens the JSON file in read mode
             to_do_list = json.load(file) #loads the JSON file into the to do list variable
+            to_do_list = {int(key): value for key, value in to_do_list.items()} #converts the keys in the to do list to integers
+            #we do this because the keys are stored as strings in the JSON file and we need to convert them back to integers
+            task_number = len(to_do_list) #gets the length of the to do list and sets it as the task number
     except FileNotFoundError: #checks if the file is not found
         console.print("[bold red]THE FILE 'user_to_do_list.json' DOES NOT EXIST![/bold red]\n") #prints a message to the user that the file does not exist
     console.print(f"[bold green]Your 'To Do List' has been loaded from the JSON file called 'user_to_do_list.json[/bold green]'\n") #prints a message to the user that the to do list has been loaded from the JSON file
@@ -192,6 +220,11 @@ def update_priority(): #function to update the priority of a task in the to do l
         new_priority = Prompt.ask("Enter the new priority of your task", choices = [str(i) for i in range(1,11)]) #asks the user to input the new priority of the task
         console.print("\n") #prints a new line
         to_do_list[int(task_number)][0] = new_priority #updates the priority of the task in the to do list with the new priority
-        
+
+def track_task_list_number(): #function to track the task list number
+    global task_number #sets the task number variable as a global variable that can be used as if it was in scope
+    task_number += 1 #increments the task number by 1
+    return task_number #returns the task number
+
 if __name__ == "__main__": #checks if the script is being run directly
     main() #calls the main function to run the program
